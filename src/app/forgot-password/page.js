1c4023b3508b574b1e362a91f0a24ea9eacc8694
Abhishek <abhi120730@gmail.com>
2025-03-04 17:47:00 +0530

    4 MARCH

---
+++ src/app/forgot-password/page.js
@@ -0,0 +1,14 @@
+import Forgot from '@/component/LOGIN/Forgot'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+        <Forgot />
+      
+    </div>
+  )
+}
+
+export default page
