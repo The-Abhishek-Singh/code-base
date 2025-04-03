33afd1748331e5eb3c90260c66f337b53be7ce13
Abhishek <abhi120730@gmail.com>
2025-04-03 15:37:06 +0530

    Fixed Now

--- src/app/partner/page.js
+++ src/app/partner/page.js
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
-import Partner from "@/component/Becomeap/Partner";
+import PartnerPage from "@/components/Becomeap/Partner";
 import React from "react";
-=======
-import Partner from '@/components/Becomeap/Partner'
-import React from 'react'
->>>>>>> 1f02b09db461c739c23ff2c08c9fd9d82ba4cfa8
+
 
 const page = () => {
   return (
     <div className="overflow-x-hidden">
       <div>
-        <Partner />
+    
+     
+     <PartnerPage />
+     
       </div>
     </div>
   );
